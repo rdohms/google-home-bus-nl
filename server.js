08936a04cb824cb71faaa93fa96ccaedf8756e91
Rafael Dohms <rdohms@gmail.com>
2017-08-25 23:30:34 +0200

    PoC Implementation

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const request = require('request')
+const R = require('ramda')
+const moment = require('moment')
+const app = express()
+
+app.post('/closest-bus', function (req, res) {
+
+  request('http://v0.ovapi.nl/tpc/30008208', function (error, response, body) {
+    let data = JSON.parse(body);
+    console.log(data['30008208'].Passes);
+
+    const getNextBus = R.pipe(
+      R.values,
+      R.map(function (pass) {
+        console.log(pass);
+        return moment(pass.ExpectedDepartureTime);
+      }),
+      R.sort(function (a,b) { return a > b}),
+      R.filter(function (pass) {
+        console.log(pass.diff(moment()))
+        return pass.diff(moment()) > 0;
+      }),
+      R.head
+    );
+
+    let next_time = moment(getNextBus(data['30008208'].Passes));
+
+    console.log(next_time);
+    res.send(`Bus 65 will depart ${next_time.fromNow()} at ${next_time.format('hh:mm:ss')}`);
+  });
+
+
+
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
